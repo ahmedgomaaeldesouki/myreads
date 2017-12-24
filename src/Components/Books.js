995a1785fd19bf5814af5b5ed6a8070bcd7b8793
ahmedgomaaeldesouki <ahmedgomaa@gmail.com>
2017-12-24 13:09:17 -0800

    Implementing code reviwer comments

--- src/Components/Books.js
+++ src/Components/Books.js
@@ -1,54 +1,57 @@
-import React, { Component } from 'react';
-import { PropTypes }  from 'prop-types'
+import React from "react";
+import { PropTypes } from "prop-types";
 
- class Book extends Component {
-    
-    static propTypes = {
-        bookimage: PropTypes.object.isRequired,
-        booktitle: PropTypes.string.isRequired,
-        bookauthors: PropTypes.array,
-        shelf: PropTypes.string.isRequired,
-        onMovingBook: PropTypes.func.isRequired
-      }
+const handleShelfChange = (e, props) => {
+  props.onMovingBook(props.id, e.target.value);
+};
 
-   handleShelfChange =  (e)=>{
-        let shelf = e.target.value ; 
-        this.props.onMovingBook (this.props.id ,shelf)
-      }
+const Book = props => {
+  const bookimage = props.bookimage.smallThumbnail || props.bookimage.thumbnail;
+  return (
+    <li>
+      <div className="book">
+        <div className="book-top">
+          <div
+            className="book-cover"
+            style={{
+              width: 128,
+              height: 193,
+              backgroundImage: `url("${bookimage}")`
+            }}
+          />
+          <div className="book-shelf-changer">
+            <select
+              onChange={e => handleShelfChange(e, props)}
+              value={props.shelf}
+            >
+              <option value="none" disabled>
+                Move to...
+              </option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option value="none">None</option>
+            </select>
+          </div>
+        </div>
+        <div className="book-title">{`${props.booktitle}`}</div>
+        {props.authors &&
+          props.authors.map((author, index) => (
+            <div className="book-authors" key={index}>
+              {`${author}`}
+            </div>
+          ))}
+      </div>
+    </li>
+  );
+};
 
-    render() {
+Book.propTypes = {
+  bookimage: PropTypes.object.isRequired,
+  booktitle: PropTypes.string.isRequired,
+  bookauthors: PropTypes.array,
+  shelf: PropTypes.string.isRequired,
+  onMovingBook: PropTypes.func.isRequired
+};
 
-       const bookimage = this.props.bookimage.smallThumbnail || this.props.bookimage.thumbnail
-     
-        return (
-          <li>
-            <div className="book">
-              <div className="book-top">
-                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${bookimage}")` }}></div> 
-                  <div className="book-shelf-changer">
-                      <select onChange={this.handleShelfChange} defaultValue={this.props.shelf} >   
-                        <option value="none" disabled>Move to...</option>
-                        <option value="currentlyReading">Currently Reading</option>
-                        <option value="wantToRead">Want to Read</option>
-                        <option value="read">Read</option>
-                        <option value="none">None</option>
-                      </select>
-                  </div>
-                </div>
-                <div className="book-title">{`${this.props.booktitle}`}</div>
-                {this.props.authors && this.props.authors.map((author,index)=>(
-                  <div
-                    className="book-authors"
-                    key={index}
-                  >{`${author}`}
-                  </div>
-                ))}
-              </div>
-          </li>
-        );
-    }
-}
-
-
-
-export default Book
+export default Book;
--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,115 +1,112 @@
-import React, { Component } from 'react';
-import {Link} from 'react-router-dom'
-import {PropTypes} from 'prop-types'
-import * as BooksAPI from '../utils/BooksAPI'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { PropTypes } from "prop-types";
+import { Debounce } from "react-throttle";
+import * as BooksAPI from "../utils/BooksAPI";
 
 //Components
-import BookShelf from './BookShelf'
+import BookShelf from "./BookShelf";
 
 class Search extends Component {
-
-    static propTypes = {
-        myBooks: PropTypes.arrayOf(PropTypes.shape({
-          title: PropTypes.string.isRequired,
-          shelf: PropTypes.string.isRequired,
-          imageLinks: PropTypes.object.isRequired,
-          authors: PropTypes.arrayOf(PropTypes.string.isRequired),
-          id: PropTypes.string.isRequired
-        })),
-        onMovingBook: PropTypes.func.isRequired
-      }
+  static propTypes = {
+    myBooks: PropTypes.arrayOf(
+      PropTypes.shape({
+        title: PropTypes.string.isRequired,
+        shelf: PropTypes.string.isRequired,
+        imageLinks: PropTypes.object.isRequired,
+        authors: PropTypes.arrayOf(PropTypes.string.isRequired),
+        id: PropTypes.string.isRequired
+      })
+    ),
+    onMovingBook: PropTypes.func.isRequired
+  };
 
   state = {
     books: [],
-    query: ''
-    }
-   
-    
-    updateQuery = (e) => {
-        const queryString = e.target.value.trim()
-        this.setState({ query: queryString })
-        
-        if (queryString !=='') {
-            this.searchBooks(queryString)
-         }  // if queryString
-      } // updateQuery
-    
-     clearState = () => {
-    this.setState({ query: '' },{ books: [] } )
-      }
+    query: ""
+  };
 
-    // iterating through search result to set my books shelf state
-    stateMyBooks = (queryResults,myBooks) => {
-     if (queryResults.length> 0) {
-        return queryResults.map((book)=>{
-            myBooks.every((myBook)=>{
-            if(myBook.id === book.id){
-              book.shelf = myBook.shelf
-              return false
-            }
-            book.shelf = 'none'
-            return true
-          })
-          return book
-        })
+  updateQuery = e => {
+    const queryString = e.target.value.trim();
+    this.setState({ query: queryString });
+    if (queryString !== "") {
+      this.searchBooks(queryString);
+    } // if queryString
+  }; // updateQuery
 
-        } // if 
-       
-     }
-    //Search the librray for books that matches the query string
-    searchBooks = (query) => {
-        if (query.length > 0 ) {
+  clearState = () => {
+    this.setState({ query: "" }, { books: [] });
+  };
 
-         BooksAPI.search(query,12)
-            .then ( (searchResults) => {
-              //  console.log (this.props.myBooks)
-                if (searchResults.length > 0) {
-                    searchResults = this.stateMyBooks(searchResults,this.props.myBooks)
-                    // set the books object state to the search results receved from BooksAPI
-                     this.setState ({books: searchResults})
-                }else {
-                    this.setState({books: []})
-                }
-             
-            } ) // BooksAPI
+  // iterating through search result to set my books shelf state
+  stateMyBooks = (queryResults, myBooks) => {
+    if (queryResults.length > 0) {
+      return queryResults.map(book => {
+        const myBook = myBooks.find(myBook => myBook.id === book.id);
+        book.shelf = typeof myBook !== "undefined" ? myBook.shelf : "none";
+        return book;
+      });
+    } // if
+  };
 
-        } // if (query.length > 0 )
-        else{
-
-            this.setState({books: [], query: ''})
+  //Search the librray for books that matches the query string
+  searchBooks = query => {
+    if (query.length > 0) {
+      BooksAPI.search(query, 12).then(searchResults => {
+        if (searchResults.length > 0) {
+          searchResults = this.stateMyBooks(searchResults, this.props.myBooks);
+          // set the books object state to the search results receved from BooksAPI
+          this.setState({ books: searchResults });
+        } else {
+          this.setState({ books: [] });
         }
-   
-    } //searchBooks
-
-   
-    render() {
-        const books = this.state.books
-        const query = this.state.query
-      
-     
-        return (
-          <div>
-            <div className="search-books">
-              <div className="search-books-bar">
-                <Link className="close-search" to="/">Close</Link>
-                <div className="search-books-input-wrapper">
-                  <input type="text"
-                    placeholder="Search by book title or author"
-                    value={query}
-                    onChange={this.updateQuery}
-                    />
-                </div>
-              </div>
-              <div className="search-books-results">
-                <ol className="books-grid"></ol>
-              </div>
-            </div>
-              {(query !== '' && books.length > 0 )  ? <BookShelf title="Search Results" books={books} onMovingBook={(id, shelf) => {
-                  this.props.onMovingBook(id, shelf)
-                      }}/> :<div> No results Found </div>}
-          </div>
-        )
+      }); // BooksAPI
+    } else {
+      // if (query.length > 0 )
+      this.setState({ books: [], query: "" });
     }
+  }; //searchBooks
+
+  render() {
+    const books = this.state.books;
+    const query = this.state.query;
+
+    return (
+      <div>
+        <div className="search-books">
+          <div className="search-books-bar">
+            <Link className="close-search" to="/">
+              Close
+            </Link>
+            <div className="search-books-input-wrapper">
+              <Debounce time="400" handler="onChange">
+                <input
+                  type="text"
+                  placeholder="Search by book title or author"
+                  value={this.value}
+                  onChange={this.updateQuery}
+                />
+              </Debounce>
+            </div>
+          </div>
+          <div className="search-books-results">
+            <ol className="books-grid" />
+          </div>
+        </div>
+        {query !== "" && books.length > 0 ? (
+          <BookShelf
+            title="Search Results"
+            books={books}
+            onMovingBook={(id, shelf) => {
+              this.props.onMovingBook(id, shelf);
+            }}
+          />
+        ) : (
+          <div> No results Found </div>
+        )}
+      </div>
+    );
+  }
 }
 
-export default Search
+export default Search;
